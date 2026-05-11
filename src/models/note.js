58450fe7c39f5290a141cfa63aa38d4a1c783141
Valeriia <valeriia.kravchuk.ua@gmail.com>
2026-05-11 22:03:05 +0300

    fix2

--- src/models/note.js
+++ src/models/note.js
@@ -14,7 +14,6 @@ const noteSchema = new Schema(
         },
         tag: {
             type: String,
-            required: true,
             enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
             default: 'Todo',
         },
